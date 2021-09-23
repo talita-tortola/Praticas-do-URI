8cc737cf2eb26a0fcd2e14b4ceae326a5a099c9b
Talita Tortola <62050109+talita-tortola@users.noreply.github.com>
2021-09-23 20:50:38 -0300

    Add files via upload

---
+++ 1 - Iniciante/1041.c
@@ -0,0 +1,27 @@
+// Iniciante - 1041 - Coordenadas de um Ponto
+
+#include <stdio.h>
+
+int main(){
+	
+	double x, y;
+	
+	scanf("%lf %lf", &x, &y);
+	
+	if(x > 0 && y > 0)
+		printf("Q1\n");
+	else if(x < 0 && y > 0)
+		printf("Q2\n");
+	else if(x < 0 && y < 0)
+		printf("Q3\n");
+	else if(x > 0 && y < 0)
+		printf("Q4\n");
+	else if(x == 0 && y == 0)
+		printf("Origem\n");
+	else if((x > 0 && y == 0) || (x < 0 && y == 0))
+		printf("Eixo X\n");
+	else if((x == 0 && y > 0) || (x == 0 && y < 0))
+		printf("Eixo Y\n");
+	
+	return 0;
+}
