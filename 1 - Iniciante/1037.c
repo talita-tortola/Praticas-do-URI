7b2ae9633d8c3b9c9d1fb14c3a7b4826e5ecd606
Talita Tortola <62050109+talita-tortola@users.noreply.github.com>
2021-10-25 20:06:40 -0300

    Update 1037.c

--- 1 - Iniciante/1037.c
+++ 1 - Iniciante/1037.c
@@ -11,7 +11,7 @@ int main(){
 	if(num >= 0 && num <= 25)
 		printf("Intervalo [0,25]\n");
 	else if(num > 25 && num <= 50)
-		printf("Intervlo (25,50]\n");
+		printf("Intervalo (25,50]\n");
 	else if(num > 50 && num <= 75)
 		printf("Intervalo (50,75]\n");
 	else if(num > 75 && num <= 100)
